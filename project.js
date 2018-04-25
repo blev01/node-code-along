61c7e3bf50a63858e9d378af947181a2289612cf
blev01 <kwbach7@gmail.com>
2018-04-24 23:33:03 -0400

    added parameter name route and cheer and jeer route

--- project.js
+++ project.js
@@ -3,9 +3,21 @@ var app = express();
 
 
 app.get('/howdy', function(req,res) {
-    res.end('We made it here. Howdy!');
+    res.end('We made it here. Howdy!  Finally following along well!');
 });
 
+app.get('/cheer', function(req, res) {
+    res.end('Having a great evening');
+})
+
+app.get('jeer', function(req, res) {
+    res.end('Looks like somobodys got a case of the Mondays!');
+})
+
+app.get('/greeting/:name', function(req, res) {
+    res.send("How are you " + req.params.name);
+})
+
 var server = app.listen(3000, function() {
-    console.log('Server fired up on PORT 3000');
+    console.log('Doing the damn thang on PORT 3000');
 })
